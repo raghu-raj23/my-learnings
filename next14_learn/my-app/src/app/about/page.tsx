cae598f593cec491c6cadc98150740bf58bbca4b
raghu-raj23 <raghuraj.muni@gmail.com>
2024-02-16 19:57:10 +0530

    Routing and SEO metadata

--- next14_learn/my-app/src/app/about/page.tsx
+++ next14_learn/my-app/src/app/about/page.tsx
@@ -1,3 +1,9 @@
+export const metadata = {
+	title: "About this project",
+	description: "yo",
+	// Static metadata
+};
+
 export default function About() {
 	return <h1>Welcome About!</h1>;
 }
--- next14_learn/my-app/src/app/blog/page.tsx
+++ next14_learn/my-app/src/app/blog/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {
+	title: "Blog",
+};
+
+
 export default function Bog() {
     return <h1>My Blog!</h1>
 }
