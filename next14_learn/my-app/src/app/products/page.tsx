11cfd5346f46ba4f78cb11ddccda3cf1d844c6f1
raghu-raj23 <raghuraj.muni@gmail.com>
2024-02-16 20:32:34 +0530

    Navigation in Next js

--- next14_learn/my-app/src/app/products/page.tsx
+++ next14_learn/my-app/src/app/products/page.tsx
@@ -1,10 +1,21 @@
+import Link from "next/link";
+
 export default function ProductList() {
 	return (
 		<>
+			<Link href="/">Home</Link>
 			<h1>Products Lists</h1>
-			<h2>P1</h2>
-			<h2>P2</h2>
-			<h2>P3</h2>
+			<h2>
+				<Link href="products/1">P1</Link>
+			</h2>
+			<h2>
+				<Link href="products/2">P2</Link>
+			</h2>
+			<h2>
+				<Link href="products/3" replace>
+					P3
+				</Link>
+			</h2>
 		</>
 	);
 }
